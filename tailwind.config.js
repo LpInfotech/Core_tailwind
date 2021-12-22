77a631e6140bb5726d89189e95ca5bc3e82cec9f
Mukesh <msalaria@constellant.com>
2021-12-22 19:02:58 +0530

    core_tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
 module.exports = {
   theme: {
     extend: {
+
       fontFamily: {
         'sans': '"Open Sans", sans-serif',
         montserrat: '"Montserrat", sans-serif',
       },
-      fontSize: {
-        'h1':'text-4xl',
-      }
+      backgroundImage: {
+        'relay': 'var(--orage-linear-color)',
+      },
+      boxShadow: {
+      'dark': 'var( --box-shodow-sm)',
+      'light': 'var(--box-shodow-xsm)'
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+      },
     },
   },
   content: ["./assets/**/*.{html,js}"],
